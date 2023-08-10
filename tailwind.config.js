84443b7f43af59611c13daab893fdc7a2337a769
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-08-10 13:16:42 +0200

    Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
    ],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            bg: "#c0c0ff",
+            text: "#0f0f0f",
+            primary: "#ffffff",
+            accent: "#0000ff"
+         }
+      },
    },
    plugins: [],
-}
+};
 
+// https://github.com/abbrcode/db/blob/main/org/design.jsonc
