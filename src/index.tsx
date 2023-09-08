71142d9ea4af3cd5886233d02f3c9bf937d295bc
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-09-08 18:53:17 +0200

    Rm Sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app/_index.tsx';
-import './index.scss';
+import './index.css';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
    <React.StrictMode>
