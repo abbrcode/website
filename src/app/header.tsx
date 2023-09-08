15e184976da4622876aa80593dea89cf454f4d83
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-09-08 18:42:55 +0200

    Bigger title

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,5 +1,5 @@
 export default function Header() {
-   return <h1 className='text-6xl text-center font-bold pt-10'>
+   return <h1 className='text-7xl text-center font-bold pt-10'>
       <span className='text-accent'>Abbr</span>
       <span className='text-primary'>eviations in </span>
       <span className='text-accent'>code</span>
