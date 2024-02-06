34d30d4d4ae8bba8d65a81c3455af682c82d9c63
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-02-07 00:15:49 +0100

    Main
    - Data fetching
    - Abbrs filtering

---
+++ src/islands/main.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+   import type { Abbr } from '../types/abbr';
+
+   export let abbrs: Abbr[];
+   export let degrees: any;
+
+   let query = '';
+
+   $: filteredAbbrs = abbrs.filter((abbrEntry) => {
+      if (abbrEntry.word.toLowerCase().includes(query.toLowerCase())) return abbrEntry;
+   });
+</script>
+
+<div>
+   <input
+      type="text"
+      placeholder="Search an abbr..."
+      class="rounded-md outline-none px-3 py-2 w-80 text-accent font-mono"
+      bind:value={query}
+   />
+   <div class="pt-3 text-center">{filteredAbbrs.length} abbrs</div>
+</div>
+
+<main class="flex flex-col items-center">
+   {#each filteredAbbrs as abbrEntry}
+      <div>
+         <span>{abbrEntry.word}</span>
+         {#each abbrEntry.abbrs as abbr}
+            <span>
+               • {degrees[abbr.degree]}
+               {abbr.abbr}
+               {abbr.degree === 'yellow' && `{${abbr.context}}`}
+            </span>
+         {/each}
+      </div>
+   {/each}
+</main>
