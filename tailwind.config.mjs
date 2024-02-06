6c293c87c23a32540342fcc146f68ec537936982
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-02-06 23:15:48 +0100

    Colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+   theme: {
+      extend: {
+         colors: {
+            antiflashWhite: '#f0f0f0',
+            blue: '#0000ff',
+            night: '#0f0f0f',
+            periwinkle: '#c0c0ff',
+            tropicalIndigo: '#8080ff',
+
+            bg: '#f0f0f0', // anti-flash white
+            text: '#0f0f0f', // night
+
+            primary: '#8080ff', // tropical indigo
+            secondary: '#c0c0ff', // periwinkle
+            accent: '#0000ff' //blue
+         }
+      },
+   },
+   plugins: [],
+};
