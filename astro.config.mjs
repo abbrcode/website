db6d05e5840268cad649e58616280d9fbf9978d9
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2024-11-01 18:23:07 +0100

    Rm: useless pkgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
    output: "server",
-   adapter: vercel({
-      webAnalytics: true
-   }),
+   adapter: vercel(),
    integrations: [svelte(), tailwind()],
 });
